f0382f3477c4e3dfc14a9fcf4a83e765438640dd
Paulo Barros <pg38421@uminho.pt>
2022-02-25 12:21:52 +0000

    checkout command added

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/CheckoutOrder/CheckoutOrderCommandValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+
+namespace Ordering.Application.Features.Orders.Commands.CheckoutOrder
+{
+    //Este validator é chamado pelo MediatR e é corrido andtes do CommandHandler ser chamado
+    public class CheckoutOrderCommandValidator : AbstractValidator<CheckoutOrderCommand>
+    {
+        public CheckoutOrderCommandValidator()
+        {
+            RuleFor(p => p.UserName)
+                .NotEmpty().WithMessage("{Username} is required")
+                .NotNull()
+                .MaximumLength(50).WithMessage("{Username} must not exceed 50 chars");
+
+            RuleFor(p => p.EmailAddress)
+                .NotEmpty().WithMessage("{EmailAddress} is required");
+
+            RuleFor(p => p.TotalPrice)
+                .NotEmpty().WithMessage("{TotalPrice} is requied")
+                .GreaterThan(0).WithMessage("{TotalPrice} should be greater than 0");
+        }
+    }
+}
